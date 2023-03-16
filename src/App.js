2c9b89cb606f9d15d0a68d4c4e81ef5b9b0c7e6c
waleedtarbosh <waleedtarbush@gmail.com>
2023-03-16 23:23:23 +0200

    Create Sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import LeftSide from "./Components/LeftSide/LeftSide";
 import Navbar from "./Components/Navbar/Navbar";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <div style={{ display: 'flex' }}>
+      <Sidebar/>
       <LeftSide />
+      </div>
     </div>
   );
 }
